a9748e07c521ad4dd87536e673e0e4ca8a3bc1bb
Iulian Dragos <jaguarul@gmail.com>
2015-11-06 14:50:57 +0100

    Reviewer’s comments.

--- rest-api/build.sbt
+++ rest-api/build.sbt
@@ -2,11 +2,6 @@ name := "rest-api"
 
 scalaVersion := "2.11.7"
 
-resolvers += 
-	"Websudos releases" at "https://dl.bintray.com/websudos/oss-releases/"
-
-val phantomVersion = "1.12.2"
-
 libraryDependencies ++= Seq(
 	"com.typesafe.akka" %% "akka-http-experimental" % "1.0",
 	"com.typesafe.akka" %% "akka-http-xml-experimental" % "1.0",
--- rest-api/src/main/scala/pipeline/rest/Main.scala
+++ rest-api/src/main/scala/pipeline/rest/Main.scala
@@ -1,21 +1,13 @@
 package pipeline.rest
 
-import scala.concurrent.Await
-import scala.concurrent.Future
-import scala.concurrent.duration.Duration
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
-import akka.util.ByteString
+
 import pipeline.data.PipelineCluster
-import scala.collection.JavaConverters._
-import spray.json._
-import pipeline.data._
+import pipeline.data.AlleleProtocol
 
 object Main extends App {
   implicit val system = ActorSystem("rest-api-system")
@@ -24,28 +16,29 @@ object Main extends App {
 
   import AlleleProtocol._
 
-  val route =
-    get {
-      path(Segment / Segment) { (population, chromosome) =>
-        parameters("start".as[Int], "end".as[Int]) { (start, end) =>
-          complete {
-            PipelineCluster.rangeQuery(population, chromosome, start, end)
+  try {
+    val route =
+      get {
+        path(Segment / Segment) { (population, chromosome) =>
+          parameters("start".as[Int], "end".as[Int]) { (start, end) =>
+            complete {
+              PipelineCluster.rangeQuery(population, chromosome, start, end)
+            }
           }
-        }
-      } ~
-      path("all") {
-        complete {
-          PipelineCluster.allRecords()
+        } ~
+        path("all") {
+          complete {
+            PipelineCluster.allRecords()
+          }
         }
       }
 
-    }
-
-  Http().bindAndHandle(route, "localhost", 1111)
-  println("Press ENTER to terminate")
-  Console.readLine()
-
-  PipelineCluster.close()
-  system.shutdown()
-  system.awaitTermination()
+    Http().bindAndHandle(route, "localhost", 1111)
+    println("Press ENTER to terminate")
+    Console.readLine()
+  } finally {
+    PipelineCluster.close()
+    system.shutdown()
+    system.awaitTermination()
+  }
 }
